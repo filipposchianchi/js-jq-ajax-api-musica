d01e622f34e3f6931d76ef1df85ab58ffec58ee1
filippo <schianchi.filippo.93@gmail.com>
2019-11-19 14:41:33 +0100

    esercizio base

---
+++ javascript/main.js
@@ -0,0 +1,39 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo. In questo momento non è importante la parte grafica.
+
+$(document).ready(function() {
+
+	 var sorgente   = $("#entry-template").html();
+	 var sorgenteDigerita = Handlebars.compile(sorgente);
+
+	// var objRef = {title: "My New Post", body: "This is my first post!"};
+	// var elValorizzato = sorgenteDigerita(objRef);
+
+	// $(".my-container").append(elValorizzato);
+	
+
+
+	$.ajax({
+		url : "https://flynn.boolean.careers/exercises/api/array/music",
+		method : "GET",
+		success : function (data) {
+			var sorgente   = $("#entry-template").html();
+	 		var sorgenteDigerita = Handlebars.compile(sorgente);
+			for(var i =0; i<10; i++) {
+				var context = {source: data.response[i].poster, title: data.response[i].title, author: data.response[i].author, year: data.response[i].year};
+
+			var html = sorgenteDigerita(context);
+			$(".my-container").append(html);
+			}
+			
+		},
+		error : function (errore) {
+			alert("E' avvenuto un errore. "+errore);
+		}
+	});
+
+
+
+
+
+});
